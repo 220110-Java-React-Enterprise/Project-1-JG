8a8d4412c9d604166ffe6b3ffacaf2aef32f06e1
jondgons <jondgons@gmail.com>
2022-02-02 13:57:47 -0600

    using SQLType

--- orm/src/main/java/annotations/Column.java
+++ orm/src/main/java/annotations/Column.java
@@ -1,16 +1,17 @@
 package annotations;
 
+import enums.SQLType;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.sql.JDBCType;
 
 @Retention(RetentionPolicy.RUNTIME) // discover annotation via reflection
 @Target(ElementType.FIELD)          // any "table" Object should have "columns" like this
 public @interface Column {
     // SQL types supported by JDBC
-    JDBCType type();
+    SQLType type();
 
     // primary key should be set to false if not applicable to column
     boolean primaryKey() default false;
