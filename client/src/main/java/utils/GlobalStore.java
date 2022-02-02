33f74fd1626dbefc42334bdf3e6fc02dd2ebd4eb
unknown <jasongraves96@gmail.com>
2022-02-02 11:29:22 -0500

    finished object postman, cloned before updating so I got whatever changes you made earlier

--- client/src/main/java/utils/GlobalStore.java
+++ client/src/main/java/utils/GlobalStore.java
@@ -1,12 +1,38 @@
 package utils;
 
-import pojos.Accessory;
-import pojos.Item;
+import pojos.*;
 
 public class GlobalStore {
     private static DataObject obj;
 	private static Item item;
 	private static Accessory accessory;
+	private static Console console;
+	private static Controller controller;
+	private static Game game;
+
+	public static Console getConsole() {
+		return console;
+	}
+
+	public static void setConsole(Console console) {
+		GlobalStore.console = console;
+	}
+
+	public static Controller getController() {
+		return controller;
+	}
+
+	public static void setController(Controller controller) {
+		GlobalStore.controller = controller;
+	}
+
+	public static Game getGame() {
+		return game;
+	}
+
+	public static void setGame(Game game) {
+		GlobalStore.game = game;
+	}
 
 	public static Accessory getAccessory() {
 		return accessory;
