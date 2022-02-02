17d4a3bf69cbec0dea7388dc23c568c42521c661
jondgons <jondgons@gmail.com>
2022-02-02 13:40:54 -0600

    ability to start the database connection

--- orm/src/main/java/ObjectReflectionManager.java
+++ orm/src/main/java/ObjectReflectionManager.java
@@ -1,3 +1,25 @@
+import utils.ConnectionManager;
+
 public class ObjectReflectionManager {
-    
+    /**
+     * Empty default constructor.
+     */
+    public ObjectReflectionManager() {}
+
+
+    /**
+     * Start the connection with the database when given a connectionString.
+     */
+    public void startConnection(String connectionString) {
+        // see if the database is already connected
+        try {
+            ConnectionManager.getConnection();
+        } catch (IllegalStateException e) {
+            System.out.println("Connection already started!");
+            return;
+        }
+
+        // start the connection
+        ConnectionManager.connect(connectionString);
+    }
 }
