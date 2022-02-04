3b7e5ff1be8c464e9088f49ecc943ebf805d1e00
jondgons <jondgons@gmail.com>
2022-02-04 08:35:05 -0600

    jason changes + junit dependency

--- client/src/main/java/Driver.java
+++ client/src/main/java/Driver.java
@@ -1,5 +1,19 @@
+//import org.junit.Test;
+import pojos.Game;
+
 public class Driver {
     public static void main(String[] args) {
-        
+        //testing code
+        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
+        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
+        orm.jasonTestingCrap();
+
+        //end testing code
+    }
+
+    //@Test
+    public static void jasonTestingCrap(){
+//couldn't figure out how to get this to work so I moved the code into main since it was empty
+// please put the code in main back in here when/if you decide to write stuff in main
     }
 }
--- orm/src/main/java/ObjectReflectionManager.java
+++ orm/src/main/java/ObjectReflectionManager.java
@@ -1,10 +1,42 @@
 import utils.ConnectionManager;
 
+import java.lang.reflect.Field;
+
 public class ObjectReflectionManager {
+    private Object obj;//added so we can pass our objects to this
+
+    public ObjectReflectionManager(Object obj) {//added so we can construct this with our object inside
+        this.obj = obj;
+    }
+
     /**
      * Empty default constructor.
      */
     public ObjectReflectionManager() {}
+
+
+    public void jasonTestingCrap(){//there's a bunch of useful reflection examples in here
+                                    //be sure to install mvn again so this will work in your client
+        System.out.println(this.obj.getClass());// this outputs "class pojos.Game"
+        //we can use .getClass to find out what kind of object we want
+
+        try {
+
+            System.out.println(obj.getClass().getDeclaredField("genre"));//this prints out "private java.lang.String pojos.Game.genre" doesn't matter if it's empty or not
+
+            Field things = obj.getClass().getDeclaredField("genre");//replace the "genre" with whatever field we want to get the value of
+            things.setAccessible(true);
+            System.out.println(things.get(obj));//this prints out the value assigned to whatever field we just got, in this case our game's genre "awesome"
+
+
+
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {//intellij made me put the try catch here
+            e.printStackTrace();
+        }
+
+
+    }
 
 
     /**
