486ea51b04ee06e5f4df9a245000b73d6922ea3c
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2022-02-08 16:37:44 -0500

    revised update for client side

--- client/src/main/java/Driver.java
+++ client/src/main/java/Driver.java
@@ -1,45 +1,45 @@
 //import org.junit.Test;
-<<<<<<< HEAD
-import pojos.Game;
+//<<<<<<< HEAD
+//import pojos.Game;
 
 public class Driver {
     public static void main(String[] args) {
-        //testing code
-        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
-        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
-        orm.jasonTestingCrap();
+//        //testing code
+//        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
+//        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
+//        orm.jasonTestingCrap();
 
-        //end testing code
-=======
-
-
-import pojos.Game;
-import utils.DatabaseConnector;
-
-public class Driver {
-    public static void main(String[] args) {
-        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
-        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
-        orm.objectSorter();//this calls whatever the approiate code is to put in database
-        //end testing code
-
-
-
-
-        // trying to connect to database
-        orm.startConnection(DatabaseConnector.getConnectionString());
->>>>>>> e99c5c0475f635ca5ef1d8e5551fd1a989be5a6b
-    }
-
-    //@Test
-    public static void jasonTestingCrap(){
-<<<<<<< HEAD
-//couldn't figure out how to get this to work so I moved the code into main since it was empty
-// please put the code in main back in here when/if you decide to write stuff in main
-=======
-        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
-        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
-        orm.objectSorter();//this calls whatever the approiate code is to put in database
->>>>>>> e99c5c0475f635ca5ef1d8e5551fd1a989be5a6b
+//        //end testing code
+//=======
+//
+//
+//import pojos.Game;
+//import utils.DatabaseConnector;
+//
+//public class Driver {
+//    public static void main(String[] args) {
+//        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
+//        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
+//        orm.objectSorter();//this calls whatever the approiate code is to put in database
+//        //end testing code
+//
+//
+//
+//
+//        // trying to connect to database
+//        orm.startConnection(DatabaseConnector.getConnectionString());
+//>>>>>>> e99c5c0475f635ca5ef1d8e5551fd1a989be5a6b
+//    }
+//
+//    //@Test
+//    public static void jasonTestingCrap(){
+//<<<<<<< HEAD
+////couldn't figure out how to get this to work so I moved the code into main since it was empty
+//// please put the code in main back in here when/if you decide to write stuff in main
+//=======
+//        Game darkSouls2ScholarOfTheFirstSin = new Game("Dark Souls 2", true, 15.99, "PS3", "Awesome", "From Software", "Bandai Namco", 2014);
+//        ObjectReflectionManager orm = new ObjectReflectionManager(darkSouls2ScholarOfTheFirstSin);
+//        orm.objectSorter();//this calls whatever the approiate code is to put in database
+//>>>>>>> e99c5c0475f635ca5ef1d8e5551fd1a989be5a6b
     }
 }
