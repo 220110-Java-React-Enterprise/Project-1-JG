94e5852ce9bb394e8846960cc3ea58b72b5e1d05
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2022-02-09 14:23:25 -0500

    merging attempt

--- orm/src/main/java/scriptors/SQLScriptor.java
+++ orm/src/main/java/scriptors/SQLScriptor.java
@@ -47,13 +47,9 @@ public abstract class SQLScriptor {
                 result += nameCleaner(fields[i].toString()) + ")";
             }
 
-<<<<<<< HEAD
         // now we have our script and just need to build it:
         String script = firstPart + tablename + nextPart + filterColumn + parameterList;
 
-
-
-=======
             // set fields back to inaccessible
             fields[i].setAccessible(false);
         }
@@ -70,23 +66,58 @@ public abstract class SQLScriptor {
      */
     public static String buildInsertStatement(Object obj){
         String result = "";
->>>>>>> 8abef884c2b9509de773dcbcb8cd56d997fb96e9
-        
         return result;
     }
-
-
+    
     /**
      * Creates the SQL statement to delete an object from the table.
      * @param obj object to reflect upon
      * @return SQL statement for deleting an object
      */
-    public static String buildDeleteStatement(Object obj){
-        String result = "";
-        
+    public static String buildDeleteStatement(Object obj) throws MalformedTableException {
+        // check if the @Table annotation is NOT present
+        if (!obj.getClass().isAnnotationPresent(Table.class)) {
+            throw new MalformedTableException("Missing @Table annotation.");
+        }
+
+        // start the statement
+        String result = "DELETE TABLE IF NOT EXISTS ";
+
+        // retrieve the name of the table, specified in the @Table annotation on the object
+        String tableName = obj.getClass().getAnnotation(Table.class).tableName();
+
+        // add the table name to the result string
+        result += tableName + " (";
+
+        // retrieve the fields available to the class
+        Field[] fields = obj.getClass().getDeclaredFields();
+
+        // iterate through the fields
+        for (int i = 0 ; i < fields.length ; i++) {
+            //TODO need to check for @Column and add its stuff if appropriate (VARCHAR, length, etc.)
+
+            // fields set to be accessible temporarily
+            fields[i].setAccessible(true);
+
+            // add each field to the result string, comma-separated
+            if (i < fields.length - 1) {
+                result += nameCleaner(fields[i].toString()) + ", ";
+            }
+            // finish with a )
+            else {
+                result += nameCleaner(fields[i].toString()) + ")";
+            }
+
+            // now we have our script and just need to build it:
+            String script = firstPart + tablename + nextPart + filterColumn + parameterList;
+
+            // set fields back to inaccessible
+            fields[i].setAccessible(false);
+        }
+
+        // return string ready for PreparedStatement
         return result;
     }
-
 
     /**
      * Helper function that retrieves the last name from a reflection'd string.
